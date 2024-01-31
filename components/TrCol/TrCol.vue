8429d1742ef026061eb5e867664d14831b5bb6f4
virtualp1 <zininnikita309@gmail.com>
2024-01-31 17:16:56 +0900

    Eslint && prettier config

---
+++ components/TrCol/TrCol.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="tr-col">
+    <slot name="header" />
+
+    <div class="tr-col__content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" src="./TrCol.scss"></style>
