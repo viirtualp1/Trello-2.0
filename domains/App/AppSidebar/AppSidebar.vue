fb155179a62d78391ab21dace0841d45a15ecec5
virtualp1 <zininnikita309@gmail.com>
2024-02-02 20:43:30 +0900

    TR-8 Sidebar: - New design - Body scroll lock when sidebar is open

--- domains/App/AppSidebar/AppSidebar.vue
+++ domains/App/AppSidebar/AppSidebar.vue
@@ -1,23 +1,29 @@
 <template>
-  <Sidebar v-model:visible="currentIsVisible" :position="sidebarPosition">
-    <p>Trello 2.0</p>
-
-    <button
+  <Sidebar
+    ref="appSidebarRef"
+    v-model:visible="currentIsVisible"
+    :position="sidebarPosition"
+    header="Trello 2.0"
+  >
+    <tr-list-item
       v-for="(sidebarButton, idx) in sidebarButtons"
       :key="idx"
-      class="main-page__sidebar-button"
+      class="app-sidebar__button"
     >
-      <img :src="sidebarButton.icon" alt="calendar" />
+      <img :src="sidebarButton.icon" alt="calendar" width="20" height="20" />
 
       {{ sidebarButton.text }}
-    </button>
+    </tr-list-item>
   </Sidebar>
 </template>
 
 <script setup lang="ts">
+import { lock, unlock } from 'tua-body-scroll-lock'
 import { useVModel } from '@vueuse/core'
 import { useBreakpoints } from '@/domains/UI'
 import Sidebar from 'primevue/sidebar'
+import { TrListItem } from '@/domains/UI'
+import HomeIcon from '@/assets/icons/home.svg'
 import CalendarIcon from '@/assets/icons/calendar.svg'
 import NotesIcon from 'assets/icons/notes.svg'
 import TasksIcon from '@/assets/icons/tasks.svg'
@@ -36,11 +42,24 @@ const emit = defineEmits({
 const { greater } = useBreakpoints()
 const currentIsVisible = useVModel(props, 'isVisible', emit)
 
+const appSidebarRef = ref<HTMLElement | null>(null)
+
+watch(
+  () => currentIsVisible.value,
+  (v: boolean) => {
+    return v ? lock(appSidebarRef.value) : unlock(appSidebarRef.value)
+  },
+)
+
 const sidebarPosition = computed(() => {
   return greater('lg').value ? 'left' : 'right'
 })
 
 const sidebarButtons = computed(() => [
+  {
+    text: 'Главная',
+    icon: HomeIcon,
+  },
   {
     text: 'Календарь',
     icon: CalendarIcon,
