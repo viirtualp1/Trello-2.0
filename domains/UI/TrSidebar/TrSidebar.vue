fefa0921aa1d3a25ae2f561f938e64c04049636c
virtualp1 <zininnikita309@gmail.com>
2024-02-02 22:01:31 +0900

    TR-8 New Sidebar (отечественная разработка)

---
+++ domains/UI/TrSidebar/TrSidebar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="tr-sidebar" :class="[{ 'is-open': isOpen }, position]">
+    <div class="tr-sidebar__overlay"></div>
+    <div class="tr-sidebar__content">
+      <div class="tr-sidebar__header">
+        <slot name="header" />
+
+        <tr-button class="tr-sidebar__close" theme="ghost" icon @click="hide">
+          <img
+            src="@/assets/icons/close.svg"
+            alt="close"
+            width="20"
+            height="20"
+          />
+        </tr-button>
+      </div>
+
+      <div class="tr-sidebar__body">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useSidebar } from '@/domains/App'
+import { TrButton } from '~/domains/UI'
+
+defineProps({
+  position: {
+    type: String as PropType<'right' | 'left'>,
+    default: 'left',
+  },
+})
+
+const { isOpen, hide } = useSidebar()
+</script>
+
+<style lang="scss" src="./TrSidebar.scss"></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   css: ['@/assets/scss/app.scss'],
-  modules: ['nuxt-primevue'],
+  modules: ['@pinia/nuxt'],
 })
