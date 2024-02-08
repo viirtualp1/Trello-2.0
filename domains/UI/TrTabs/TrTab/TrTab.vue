2154447afcb2f48384c995a50094ea1a5e1e97f0
virtualp1 <zininnikita309@gmail.com>
2024-02-08 20:32:43 +0900

    Tabs new files

---
+++ domains/UI/TrTabs/TrTab/TrTab.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="tr-tab"></div>
+</template>
+
+<script setup lang="ts">
+import { TrButton } from '@/domains/UI'
+</script>
+
+<style lang="scss" src="./TrTab.scss"></style>
---
+++ domains/UI/TrTabs/TrTab/index.ts
@@ -0,0 +1,1 @@
+export { default as TrTab } from './TrTab.vue'
