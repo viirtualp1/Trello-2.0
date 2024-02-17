cb6e12b6ec0274a31e94feb7f7af0115a3c15c42
virtualp1 <zininnikita309@gmail.com>
2024-02-18 00:22:13 +0900

    optimize

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,5 @@
 module.exports = {
     root: true,
-    env: {
-        browser: true,
-        node: true,
-    },
-    parser: 'vue-eslint-parser',
-    parserOptions: {
-        parser: '@typescript-eslint/parser',
-        sourceType: 'module',
-    },
     extends: [
         'eslint:recommended',
         'plugin:vue/vue3-recommended',
